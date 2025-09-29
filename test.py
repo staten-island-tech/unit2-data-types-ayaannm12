cb4cf73a07042246e36ce53139a5b3ba7442b018
ayaannm12 <ayaanm5352@gmail.com>
2025-09-29 09:43:43 -0400

    w

---
+++ test.py
@@ -0,0 +1,6 @@
+tip = 100
+service = input("how was your service,bad good or great?")
+if service =="bad":print("sorry to hear that, we'll issue a 15% tip")
+if service =="good": print("great, well we'll issue a 20% tip")
+elif service == "great": print("amazing, we'll issue a 25% tip!")
+else: print("sorry thats not a valid answer please answer 'bad' 'good' or 'great'")
